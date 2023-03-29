b7cd3b3e21821bb6629b7a91e957fed403a5f315
Dwan <yingduan_ge@epam.com>
2023-03-29 14:32:43 +0800

    multi file upload

--- src/pages/BinData/index.tsx
+++ src/pages/BinData/index.tsx
@@ -1,42 +1,49 @@
 import { useState } from "react";
 
+// TODO: try multi files
 export default () => {
   // TODO: should check
-  const [image, setImage] = useState<ArrayBuffer | string | null>();
+  const [imageURLs, setImageURLs] = useState<string[]>([]);
   // event 类型 可以通过在传入的地方查看,如onChange={(e) => handleChange}, 鼠标悬在e上即知event类型
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     // FileList.item() 返回FileList索引值对应的项
     //  ?? typescript 语法，当左侧为空返回右侧 与|| 在设置默认值时用法相同
-    const file = e.target?.files?.item(0) ?? new Blob(["ERROR"]);
-    let reader = new FileReader();
-    // read the binary data and encode it as base64 data url.
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-      setImage(reader.result);
-    };
-    reader.onerror = function (error) {
-      console.log("error: ", error);
-    };
+    const files = e.target.files;
+    const images = [] as string[];
+    if (!files?.length) return;
+    // change convert way because asynchronuse
+    for (const file of files) {
+      const url = URL.createObjectURL(file);
+      console.log(url);
+      images.push(url);
+    }
+    setImageURLs(images);
   };
   return (
     <div className="w-screen">
-      <label className="block m-auto mt-10">
-        {/* input hidden 可以内嵌在label里，不能内嵌在button里 */}
-        <input
-          type="file"
-          id="imageLoader"
-          name="imageLoader"
-          accept="image/png, image/jpeg"
-          // wrong: onChange={(e) => handleChange}
-          onChange={handleChange}
-          hidden
-        />
+      <label htmlFor="imageLoader" className="block m-auto mt-10 bg-red-500">
         Upload File
       </label>
-      <div
-        className="w-48 h-48 bg-contain"
-        style={{ backgroundImage: `url(${image})` }}
-      ></div>
+      {/* input hidden 可以内嵌在label里，不能内嵌在button里 */}
+      <input
+        type="file"
+        id="imageLoader"
+        name="imageLoader"
+        accept="image/png, image/jpeg"
+        multiple
+        // wrong: onChange={(e) => handleChange}
+        onChange={handleChange}
+        hidden
+      />
+      <div className="flex">
+        {imageURLs?.map((imageURL, index) => (
+          <div
+            key={`imageURL${index}`}
+            className="w-48 h-48 bg-contain bg-no-repeat bg-center m-auto"
+            style={{ backgroundImage: `url(${imageURL})` }}
+          />
+        ))}
+      </div>
     </div>
   );
 };
